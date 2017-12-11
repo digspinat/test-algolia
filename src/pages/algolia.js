13c9c58573f9e812e50699f637cdc8eade8ff1f0
Mitesh <cmilan@pistalix.com>
2017-12-11 17:06:59 +0530

    Initial Code

--- src/pages/algolia.js
+++ src/pages/algolia.js
@@ -19,7 +19,7 @@ export default () => {
   return (
     <div className="ui container">
       <Container>
-        <InstantSearch appId="YZO8M6AZ6J" apiKey="74516bbcef74ec917e2acbcd0df2df0f" indexName="contentful-cml">
+        <InstantSearch appId="YZO8M6AZ6J" apiKey="74516bbcef74ec917e2acbcd0df2df0f" indexName="algolia-test">
           <Grid>
             <Grid.Column mobile={16} tablet={8} computer={4}>
               <Panel title="Brand">
